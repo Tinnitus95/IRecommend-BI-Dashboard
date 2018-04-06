eb3fd13536fcd328e0b7df6345667eca6908e8e6
Mikael Gustafsson <mikael.gustafsson@student.kyh.se>
2018-04-06 12:29:24 +0200

    add bootstrap mockup

---
+++ mockup/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $(this).toggleClass('active');
+    });
+});
+
+let listItems = document.querySelectorAll('li');
+
+
+
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
