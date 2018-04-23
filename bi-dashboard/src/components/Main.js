0316b0c8b8c57ccdc4e599764b396f47f7b207f8
Mikael Gustafsson <mikael.gustafsson@student.kyh.se>
2018-04-23 14:25:00 +0200

    add numberwidgets with data from db fetch

--- bi-dashboard/src/components/Main.js
+++ bi-dashboard/src/components/Main.js
@@ -5,41 +5,12 @@ import NumberWidget from './NumberWidget';
 
 
 class Main extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-             numbers:[
-            //{
-            //     name:'Antal Tips',
-            //     amount: 200
-            // },
-            // {
-            //     name:'Intresserade rekommendationer',
-            //     amount: 200
-            // },
-            // {
-            //     name:'Intervjuade kandidater',
-            //     amount: 200
-            // },
-            // {
-            //     name:'Tips som lett till anställning',
-            //     amount: 200
-            // },
-            // {
-            //     name:'Tips per person',
-            //     amount: 200
-            // }
-        ]
-        }
-    }
     render(){
         return(
             <div>
                 <SideNav/>
                 <div className="content">
-                    <NumberWidget
-                        numbers={this.state.numbers}
-                    />
+                    <NumberWidget />
                 </div>
             </div>
         );
