b68a170562fd381edd0b7faecf739cdc1cac0b08
FadiGo <fadi.gourie@student.kyh.se>
2018-05-16 10:55:09 +0200

    Fix translations & translate sidebar menu

--- bi-dashboard/src/Index.js
+++ bi-dashboard/src/Index.js
@@ -5,5 +5,19 @@ import App from './components/App';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
+import { IntlProvider, addLocaleData } from 'react-intl';
+import { getLanguage } from './api';
 
-ReactDOM.render(<App />, document.getElementById('app'))
+import enLocaleData from 'react-intl/locale-data/en';
+import svLocaleData from 'react-intl/locale-data/sv';
+import translations from './translations';
+
+addLocaleData([...svLocaleData, ...enLocaleData]);
+const language = getLanguage();
+
+
+
+ReactDOM.render(<IntlProvider locale={language} messages={translations[language]}>
+                    <App />
+                </IntlProvider>,
+                 document.getElementById('app'));
