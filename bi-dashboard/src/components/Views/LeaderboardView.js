48cb0041314f51c060bfb565098e7fd34bcfa93e
Tinnitus95 <qiusheng95@gmail.com>
2018-05-23 21:58:07 +0200

    fix top-three orbs with Mikael

--- bi-dashboard/src/components/Views/LeaderboardView.js
+++ bi-dashboard/src/components/Views/LeaderboardView.js
@@ -1,12 +1,43 @@
 import React, {Component} from 'react';
 import UserOrbs from '../UserOrbs';
-
+import _ from 'lodash';
 
 class LeaderboardView extends Component {
+
+
+    renderTop(){
+        const {userscore} = this.props.data;
+        const topThree = userscore.slice(0,3);
+
+        for (var i = 0; i < topThree.length; i++) {
+
+            if( i === 0 ){
+                topThree[i].classes = 'leader';
+            }
+            else{
+                topThree[i].classes = 'not-leader'
+            }
+            topThree[i].position = i +1;
+
+
+        }
+        console.log(topThree);
+        return _.map(topThree, top => {
+            return <UserOrbs
+                key= {top.id}
+                top= {top}
+
+            />;
+        })
+    }
     render(){
+
+
         return (
             <div className="leaderboard-view">
-                hello
+                <div className="top-three">
+                    {this.renderTop()}
+                </div>
             </div>
         );
     }
