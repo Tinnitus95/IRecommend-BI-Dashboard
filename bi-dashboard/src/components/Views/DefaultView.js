158d83b1343469c1313d73d5617c06d036e67a7c
Tinnitus95 <qiusheng95@gmail.com>
2018-05-28 12:43:03 +0200

    remove sleep function

--- bi-dashboard/src/components/Views/DefaultView.js
+++ bi-dashboard/src/components/Views/DefaultView.js
@@ -7,16 +7,6 @@ import LineCharts from '../LineCharts';
 
 
 export default class DefaultView extends Component{
-
-     sleep = (milliseconds) => {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
-
     render(){
         const data = this.props.data;
         //this.sleep(1000);
