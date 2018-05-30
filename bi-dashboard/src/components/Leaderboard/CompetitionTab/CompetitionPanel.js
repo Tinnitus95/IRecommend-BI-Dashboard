422898df4161538a706f6ff56ee613fce480cd5d
FadiGo <fadi.gourie@student.kyh.se>
2018-05-30 11:54:25 +0200

    add styling

--- bi-dashboard/src/components/Leaderboard/CompetitionTab/CompetitionPanel.js
+++ bi-dashboard/src/components/Leaderboard/CompetitionTab/CompetitionPanel.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class CompetitionPanel extends Component {
     render() {
         return(
-            <progress max="100" value="10"></progress>
+            <progress 
+                    max="100" 
+                    value="10">
+            </progress>
         );
     }
 };
