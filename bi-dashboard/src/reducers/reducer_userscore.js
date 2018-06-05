b9335a6c42d2951ee62c15a8f3f979bfef8d4a77
Mikael Gustafsson <mikael.gustafsson@student.kyh.se>
2018-06-05 09:54:46 +0200

    resolved discard error

--- bi-dashboard/src/reducers/reducer_userscore.js
+++ bi-dashboard/src/reducers/reducer_userscore.js
@@ -4,6 +4,7 @@ export default function(state = [], action){
     switch (action.type) {
         case FETCH_USERSCORE:
         const data = action.payload.data;
+
         const user = data.map( user => {
           return(
             {
@@ -12,6 +13,7 @@ export default function(state = [], action){
               firstname: user.user.givenname,
               picture: user.user.picture,
               points: user.points.Int64,
+              teamhref: user.user.team.href
             }
           );
         });
